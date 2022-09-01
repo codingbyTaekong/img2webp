8d5b07c8ca74d0413d7cac51f5fe1fc114298c59
dev_t.h <kumi7214@gmail.com>
2022-09-01 14:25:22 +0900

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+import imagemin from 'imagemin';
+import imageminJpegtran from 'imagemin-jpegtran';
+import imageminPngquant from 'imagemin-pngquant';
+import imageminWebp from 'imagemin-webp';
+
+(async ()=>{
+    const files = await imagemin(['sources/*.{jpg,png}'], {
+        destination: 'build/cafes_',
+        plugins: [
+            // imageminJpegtran(),
+            // imageminPngquant({
+            // 	quality: [0.6, 0.8]
+            // })
+            imageminWebp()
+        ]
+    });
+})();
